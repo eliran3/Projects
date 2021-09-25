c8d6cf282e0311c1d6bf3412acc1d9cede12dbda
eliranCoding <58547263+eliranCoding@users.noreply.github.com>
2021-09-25 23:12:37 +0300

    Add files via upload

---
+++ Ipscanner.py
@@ -0,0 +1,55 @@
+#! /usr/bin/python3
+from scapy.all import *
+import threading, netifaces
+from colorama import Fore
+from time import sleep
+
+ips = []
+
+subdomain = get_gateway()[:len(get_gateway())-2]
+
+stop_sniffer = threading.Event()
+
+thread = socket = None
+
+def get_iface():
+    return netifaces.gateways()['default'][netifaces.AF_INET][1]
+
+def get_gateway():
+    return netifaces.gateways()['default'][netifaces.AF_INET][0]
+
+def sniffing():
+    socket = conf.L2listen(type=ETH_P_ALL, iface=get_iface())
+    sniff(prn=find_ips, opened_socket= socket, timeout=30, quiet=True, stop_filter=should_stop_sniffer, lfilter=lambda pkt: 'IP' in pkt)
+
+def should_stop_sniffer(packet):
+    return stop_sniffer.isSet()
+
+def find_ips(packet):
+    if packet['IP'].src.startswith(subdomain) and packet['IP'].src not in ips:
+        ips.append(packet['IP'].src)
+    if packet['IP'].dst.startswith(subdomain) and packet['IP'].dst not in ips:
+        ips.append(packet['IP'].dst)
+
+def perform_ip_scan():
+    #global thread
+
+    if thread is None or not thread.is_alive():
+        stop = False
+        thread = threading.Thread(target=sniffing)
+        thread.start()
+
+def main():
+    perform_ip_scan()
+
+    try:
+        while True:
+            sleep(100)
+    except KeyboardInterrupt:
+        stop_sniffer.set()
+        if thread.is_alive():
+            socket.close()
+        print(f'\n{ips}')
+
+if __name__ = '__main__':
+    main()
