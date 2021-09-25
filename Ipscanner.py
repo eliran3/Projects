822771707c861f101deb51dd6339ab9e94c8f286
eliranCoding <58547263+eliranCoding@users.noreply.github.com>
2021-09-25 23:32:52 +0300

    Update Ipscanner.py

--- Ipscanner.py
+++ Ipscanner.py
@@ -1,10 +1,14 @@
 #! /usr/bin/python3
+
 from scapy.all import *
 import threading, netifaces
 from colorama import Fore
 from time import sleep
 
 ips = []
+
+def get_gateway():
+    return netifaces.gateways()['default'][netifaces.AF_INET][0]
 
 subdomain = get_gateway()[:len(get_gateway())-2]
 
@@ -15,12 +19,10 @@ thread = socket = None
 def get_iface():
     return netifaces.gateways()['default'][netifaces.AF_INET][1]
 
-def get_gateway():
-    return netifaces.gateways()['default'][netifaces.AF_INET][0]
-
 def sniffing():
     socket = conf.L2listen(type=ETH_P_ALL, iface=get_iface())
     sniff(prn=find_ips, opened_socket= socket, timeout=30, quiet=True, stop_filter=should_stop_sniffer, lfilter=lambda pkt: 'IP' in pkt)
+    print(Fore.BLUE + 'Finished scanning!')
 
 def should_stop_sniffer(packet):
     return stop_sniffer.isSet()
@@ -32,7 +34,9 @@ def find_ips(packet):
         ips.append(packet['IP'].dst)
 
 def perform_ip_scan():
-    #global thread
+    global thread
+
+    print(Fore.BLUE + 'Performing ip scan...')
 
     if thread is None or not thread.is_alive():
         stop = False
@@ -47,9 +51,11 @@ def main():
             sleep(100)
     except KeyboardInterrupt:
         stop_sniffer.set()
+        
         if thread.is_alive():
             socket.close()
-        print(f'\n{ips}')
+        
+        print(Fore.BLUE + f'\n{ips}')
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
